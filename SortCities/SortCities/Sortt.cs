f5b11176141748616742023216e1329277d07fb9
Settheworldonfireiii <zilyardoff.foton@yandex.ru>
2017-01-15 18:01:17 +0300

    Add files via upload
    Version 1

---
+++ SortCities/SortCities/Sortt.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortCities
+{
+    public class Sortt
+    {   
+        //сложность алгоритма в худшем случае(когда первый в изначальном списке элемент - последний перелёт)-O(n^2)
+        //в лучшем случае (когда все элементы, кроме небольшого количества, итак упорядочены в порядке перелётов) - O(n)
+        //в среднем ближе к O(n^2)
+        public static List<Tuple<String, String>> SortCitiesFunct(List<Tuple<String, String>> inilist)
+        {
+            foreach (Tuple<String, String> tpl in inilist)
+            {
+                if ((tpl.Item1 == null)|| (tpl.Item2 == null)||(tpl.Item1 == "")||(tpl.Item2 == "")) {
+                    throw new EmptyField("one or more of the fields is empty");
+                }
+            }
+            var mylst = new List<Tuple<String, String>>();
+            //добавляем в отсортированный список два первых элемента, далее будем добавлять по одному элементу
+            //чтобы не пришлось в цикле каждый раз проверять, не является ли данная итерация первой
+            FirstInInit(inilist, mylst);
+            SortAfterInitFirst(inilist, mylst);
+
+            //достраиваем те элементы, которые идут до первого элемента изначального, несортированного списка
+            SortBeforeInitFirst(inilist, mylst);
+            return mylst;
+         }
+
+
+
+
+        //добавляем в отсортированный список два первых элемента, потому что далее будем добавлять по одному элементу
+        public static void FirstInInit(List<Tuple<String, String>> inilist, List<Tuple<String, String>> mylst)
+        {
+            for (int k = 1; k < inilist.Count; k++)
+            {
+                Tuple<String, String> tpl;
+                tpl = inilist[k];
+                if (inilist[0].Item2.Equals(tpl.Item1, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    mylst.Add(inilist[0]);
+                    mylst.Add(tpl);
+                    inilist.RemoveAt(0);
+                    k--;
+                    inilist.RemoveAt(k);
+                    break;
+
+                }
+            }
+        }
+
+
+        //вначале располагаем в нужном порядке те элементы, которые будут следовать после элемента,
+        //который был первым в изначальном, несортированном списке
+        public static void SortAfterInitFirst(List<Tuple<String, String>> inilist, List<Tuple<String, String>> mylst)
+        {
+            for (int i = 0; i < mylst.Count; i++)
+            {
+                Tuple<String, String> tpl = mylst[i];
+                for (int k = 0; k < inilist.Count; k++)
+                {
+                    Tuple<String, String> tpl_inner = inilist[k];
+                    if (tpl.Item2.Equals(tpl_inner.Item1, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        mylst.Add(tpl_inner);
+                        inilist.RemoveAt(k);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        //достраиваем те элементы, которые идут до первого элемента изначального, несортированного списка
+        public static void SortBeforeInitFirst(List<Tuple<String, String>> inilist, List<Tuple<String, String>> mylst)
+        {
+            while (inilist.Count != 0)
+            {
+                Tuple<String, String> tpl = mylst[0];
+                for (int k = 0; k < inilist.Count; k++)
+                {
+
+                    Tuple<String, String> tpl_inner = inilist[k];
+                    if (tpl.Item1.Equals(tpl_inner.Item2, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        mylst.Insert(0, tpl_inner);
+                        inilist.RemoveAt(k);
+                        break;
+                    }
+
+                }
+            }
+
+        }
+        //вывод
+        public static void outPut(List<Tuple<String, String>> mylst)
+        {
+            foreach (Tuple<String, String> tpl in mylst)
+            {
+                Console.WriteLine(tpl.Item1 + ' ' + tpl.Item2);
+            }
+        }
+
+
+
+    }
+    //исключение в случае пустого или нулевого ввода
+    public class EmptyField : Exception
+    {
+        public EmptyField():base() { }
+        public EmptyField(string message): base(message) { }
+    } 
+}
+
+
+
+
+
---
+++ SortCities/SortTaTest/SortCitiesTest.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortCities;
+using System.Collections.Generic;
+
+namespace SortTestProj
+{
+    [TestClass]
+    public class SortCitiesTest
+    {
+        //тестируем на пустой ввод
+        [TestMethod]
+        [ExpectedException(typeof(EmptyField), "one or more of the fields is empty or null")]
+        public void TestEmptyInput()
+        {
+            //Arrange
+            Sortt sorttobj = new Sortt();
+            var res = new List<Tuple<String, String>>();
+            var inittestdata = new List<Tuple<String, String>>()
+            {
+                Tuple.Create("Tel Aviv", "Ottawa"),
+                Tuple.Create("", "Mombasa"),
+                Tuple.Create("Kuala Lumpur", "New York")
+            };
+            //Act
+            res = Sortt.SortCitiesFunct(inittestdata);
+          }
+
+        //тестируем на нулевой ввод
+        [TestMethod]
+        [ExpectedException(typeof(EmptyField), "one or more of the fields is empty or null")]
+        public void TestNullInput()
+        {
+            //Arrange
+            Sortt sorttobj = new Sortt();
+            var res = new List<Tuple<String, String>>();
+            var tuple = Tuple.Create<string, string>(null, null);
+            var inittestdata = new List<Tuple<String, String>>()
+            {
+                Tuple.Create("Tel Aviv", "Ottawa"),
+                tuple,
+                Tuple.Create("Kuala Lumpur", "New York")
+            };
+            //Act
+            res = Sortt.SortCitiesFunct(inittestdata);
+        }
+
+        //тестируем всю функцию
+        [TestMethod]
+        public void SortCitiesFunctTest()
+        {
+            //Arrange
+            Sortt sorttobj = new Sortt();
+            var inittestdata = new List<Tuple<String, String>>()
+            {
+                Tuple.Create("Tel Aviv", "Ottawa"),
+                Tuple.Create("Moscow", "Mombasa"),
+                Tuple.Create("Kuala Lumpur", "New York"),
+                Tuple.Create("Minsk","Moscow"),
+                Tuple.Create("New York", "Bangalore"),
+                Tuple.Create("San Francisco", "Shenzhen"),
+                Tuple.Create("Bangalore", "Tel Aviv"),
+                Tuple.Create("Mombasa", "Kuala Lumpur"),
+                Tuple.Create("Ottawa", "San Francisco"),
+            };
+            var rightresult = new List<Tuple<String, String>>()
+           {
+               Tuple.Create("Minsk","Moscow"),
+               Tuple.Create("Moscow", "Mombasa"),
+               Tuple.Create("Mombasa", "Kuala Lumpur"),
+               Tuple.Create("Kuala Lumpur", "New York"),
+               Tuple.Create("New York", "Bangalore"),
+               Tuple.Create("Bangalore", "Tel Aviv"),
+               Tuple.Create("Tel Aviv", "Ottawa"),
+               Tuple.Create("Ottawa", "San Francisco"),
+               Tuple.Create("San Francisco", "Shenzhen")
+           };
+            //Act
+            var result = Sortt.SortCitiesFunct(inittestdata);
+            //Assert
+            CollectionAssert.AreEquivalent(rightresult, result, "the test failed");
+        }
+
+        //тестируем первую составляющую
+        [TestMethod]
+        public void FirstInInitTest()
+        {
+            //Arrange
+            Sortt sorttobj = new Sortt();
+            var result_mylst = new List<Tuple<String, String>>();
+            var inittestdata = new List<Tuple<String, String>>()
+            {
+                Tuple.Create("Tel Aviv", "Ottawa"),
+                Tuple.Create("Moscow", "Mombasa"),
+                Tuple.Create("Kuala Lumpur", "New York"),
+                Tuple.Create("Minsk", "Moscow"),
+                Tuple.Create("New York", "Bangalore"),
+                Tuple.Create("San Francisco", "Shenzhen"),
+                Tuple.Create("Bangalore", "Tel Aviv"),
+                Tuple.Create("Mombasa", "Kuala Lumpur"),
+                Tuple.Create("Ottawa", "San Francisco")
+            };
+            var rightresult = new List<Tuple<String, String>>()
+           {
+               Tuple.Create("Tel Aviv", "Ottawa"),
+               Tuple.Create("Ottawa", "San Francisco")
+           };
+            //Act
+            Sortt.FirstInInit(inittestdata, result_mylst);
+
+            //Assert
+            CollectionAssert.AreEquivalent(rightresult, result_mylst, "the test failed");
+        }
+
+        //тестируем вторую составляющую
+        [TestMethod]
+        public void SortAfterInitFirstTest()
+        {
+            //Arrange
+            Sortt sorttobj = new Sortt();
+            var result_mylst = new List<Tuple<String, String>>()
+            {
+               Tuple.Create("Tel Aviv", "Ottawa"),
+               Tuple.Create("Ottawa", "San Francisco")
+            };
+            var inittestdata = new List<Tuple<String, String>>()
+            {
+                Tuple.Create("Moscow", "Mombasa"),
+                Tuple.Create("Kuala Lumpur", "New York"),
+                Tuple.Create("Minsk", "Moscow"),
+                Tuple.Create("New York", "Bangalore"),
+                Tuple.Create("San Francisco", "Shenzhen"),
+                Tuple.Create("Bangalore", "Tel Aviv"),
+                Tuple.Create("Mombasa", "Kuala Lumpur"),
+            };
+            var rightresult = new List<Tuple<String, String>>()
+           {
+               Tuple.Create("Tel Aviv", "Ottawa"),
+               Tuple.Create("Ottawa", "San Francisco"),
+               Tuple.Create("San Francisco", "Shenzhen")
+           };
+            //Act
+            Sortt.SortAfterInitFirst(inittestdata, result_mylst);
+
+            //Assert
+            CollectionAssert.AreEquivalent(rightresult, result_mylst, "the test failed");
+        }
+
+        //тестируем третью составляющую
+        [TestMethod]
+        public void SortBeforeInitFirstTest()
+        {
+            //Arrange
+            Sortt sorttobj = new Sortt();
+            var result_mylst = new List<Tuple<String, String>>()
+            {
+               Tuple.Create("Tel Aviv", "Ottawa"),
+               Tuple.Create("Ottawa", "San Francisco"),
+               Tuple.Create("San Francisco", "Shenzhen")
+            };
+            var inittestdata = new List<Tuple<String, String>>()
+            {
+                Tuple.Create("Moscow", "Mombasa"),
+                Tuple.Create("Kuala Lumpur", "New York"),
+                Tuple.Create("Minsk", "Moscow"),
+                Tuple.Create("New York", "Bangalore"),
+                Tuple.Create("Bangalore", "Tel Aviv"),
+                Tuple.Create("Mombasa", "Kuala Lumpur"),
+            };
+            var rightresult = new List<Tuple<String, String>>()
+           {
+               Tuple.Create("Minsk","Moscow"),
+               Tuple.Create("Moscow", "Mombasa"),
+               Tuple.Create("Mombasa", "Kuala Lumpur"),
+               Tuple.Create("Kuala Lumpur", "New York"),
+               Tuple.Create("New York", "Bangalore"),
+               Tuple.Create("Bangalore", "Tel Aviv"),
+               Tuple.Create("Tel Aviv", "Ottawa"),
+               Tuple.Create("Ottawa", "San Francisco"),
+               Tuple.Create("San Francisco", "Shenzhen")
+           };
+            //Act
+            Sortt.SortBeforeInitFirst(inittestdata, result_mylst);
+
+            //Assert
+            CollectionAssert.AreEquivalent(rightresult, result_mylst, "the test failed");
+        }
+
+    }
+}
